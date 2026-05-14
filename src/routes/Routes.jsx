3bb35e8ba4611b415a45d9dca04fb298f4fe9606
akib-naimul <akibnaimul2000@gmail.com>
2026-05-14 22:28:07 +0600

    fix: wrap ProductDetails and ManageOrders routes in PrivateRoute

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,189 @@
+import { createBrowserRouter } from 'react-router'
+import Home from '../pages/Home/Home'
+import ErrorPage from '../pages/ErrorPage'
+import Login from '../pages/Login/Login'
+import SignUp from '../pages/SignUp/SignUp'
+import PrivateRoute from './PrivateRoute'
+import DashboardLayout from '../layouts/DashboardLayout'
+import ManageUsers from '../pages/Dashboard/Admin/ManageUsers'
+import Profile from '../pages/Dashboard/Common/Profile'
+import Statistics from '../pages/Dashboard/Common/Statistics'
+import MainLayout from '../layouts/MainLayout'
+import ManageOrders from '../pages/Dashboard/Manager/ManageOrders'
+import BookingPage from '../pages/BookingPage/BookingPage'
+import ProductDetails from '../pages/ProductDetails/ProductDetails'
+import AddProduct from '../pages/Dashboard/Manager/AddProduct'
+import AllProducts from '../pages/AllProducts/AllProducts'
+import About from '../pages/About/About'
+import Contact from '../pages/Contact/Contact'
+import MyOrders from '../pages/Dashboard/Buyer/MyOders'
+import TrackOrder from '../pages/Dashboard/Buyer/TrackOrder'
+import AdminAllProducts from '../pages/Dashboard/Admin/AllProducts'
+import AdminAllOrders from '../pages/Dashboard/Admin/AllOrders'
+import ApprovedOrders from '../pages/Dashboard/Manager/ApprovedOrders'
+import UpdateProduct from '../pages/Dashboard/Manager/UpdateProduct'
+import Privacy from '../pages/Privacy/Privacy'
+import AIKnowledgePanel from '../pages/Dashboard/Admin/AIKnowledgePanel'
+
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/products',
+        element: <AllProducts />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact/>,
+      },
+      {
+        path: '/privacy',
+        element: <Privacy/>,
+      },
+      {
+        path: '/products/:id',
+        element: <PrivateRoute><ProductDetails /></PrivateRoute>,
+      },
+      {
+        path: '/products/booking/:id',
+        element: <PrivateRoute><BookingPage /></PrivateRoute>,
+      },
+    ],
+  },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+  {
+    path: '/dashboard',
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <PrivateRoute>
+            <Statistics />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'add-product',
+        element: (
+          <PrivateRoute>
+            <AddProduct />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'add-plant',
+        element: (
+          <PrivateRoute>
+            <AddProduct />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'manage-products',
+        element: (
+          <PrivateRoute>
+            <UpdateProduct />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'manage-users',
+        element: (
+          <PrivateRoute>
+            <ManageUsers />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'all-products',
+        element: (
+          <PrivateRoute>
+            <AdminAllProducts />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'all-orders',
+        element: (
+          <PrivateRoute>
+            <AdminAllOrders />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'ai-knowledge',
+        element: (
+          <PrivateRoute>
+            <AIKnowledgePanel/>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'profile',
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'my-orders',
+        element: (
+          <PrivateRoute>
+            <MyOrders/>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'approved-orders',
+        element: (
+          <PrivateRoute>
+            <ApprovedOrders/>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'track-order/:orderId',
+        element: (
+          <PrivateRoute>
+            <TrackOrder/>
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'manage-orders',
+        element: (
+          <PrivateRoute>
+            <ManageOrders />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: 'pending-orders',
+        element: (
+          <PrivateRoute>
+            <ManageOrders />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+])
